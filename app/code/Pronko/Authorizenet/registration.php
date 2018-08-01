9d10501510be6cbd7b948fbefc66c33462c28e7e
thomasnordkvist <thomas.nordkvist@gmail.com>
2018-08-01 16:29:19 +0200

    Create

---
+++ app/code/Pronko/Authorizenet/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomas
+ * Date: 2018-08-01
+ * Time: 15:33
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Pronko_Authorizenet',
+    __DIR__
+);
